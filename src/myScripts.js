69e8d4dd54db0bc5e10b7913171e3c95af477715
Ignacio Rondini <jirondini@gmail.com>
2018-03-27 09:33:46 +0200

    Multiple circles and bullet addition

--- src/myScripts.js
+++ src/myScripts.js
@@ -1,65 +1,123 @@
 
+var spaceShipParams = {
+	life:100.0
+};
+globalParameters = {
+	initiated:false
+};
 
-            function leftArrowPressed() {
-                var element = $('#spaceShip');
-            	var left = parseInt(element.css("left")) - 5;
-            	$('#spaceShip').css('left',left);
+function leftArrowPressed() {
+    var element = $('#spaceShip');
+    var left = parseInt(element.css("left")) - 5;
+    $('#spaceShip').css('left',left);
 
                 }
 
-            function rightArrowPressed() {
-                var element = $('#spaceShip');
-                var left = parseInt(element.css("left")) + 5;
-                $('#spaceShip').css('left',left);
-            }
+function rightArrowPressed() {
+    var element = $('#spaceShip'); 
+    var left = parseInt(element.css("left")) + 5;
+    $('#spaceShip').css('left',left);
+}
 
-            function upArrowPressed() {
-                var element = $('#spaceShip');
-                var top = parseInt(element.css("top")) - 5;
-                $('#spaceShip').css('top',top);
-            }
+function upArrowPressed() {
+    var element = $('#spaceShip');
+    var top = parseInt(element.css("top")) - 5;
+    $('#spaceShip').css('top',top);
+}
 
-            function downArrowPressed() {
-                var element = $('#spaceShip');
-                var top = parseInt(element.css("top")) + 5;
-                $('#spaceShip').css('top',top);
-            }
+function downArrowPressed() {
+    var element = $('#spaceShip');
+    var top = parseInt(element.css("top")) + 5;
+    $('#spaceShip').css('top',top);
+}
 
-            function moveSelection(event) {
+function spaceKeyPressed(){
+    var container = $('#container');
+    // add bullet
+    container.append('<div class = "bullet"></div>');
+    var newBullet = $('.bullet').eq(-1);
+    var spaceShip = $('#spaceShip');
+	newBullet.css("top",spaceShip.css("top"));
+	newBullet.css("left",spaceShip.css("left"));
+	newBullet.css("right",spaceShip.css("right"));
+	
+}
 
-                switch (event.keyCode) {
-                    case 37:
-                       leftArrowPressed();
-                    break;
+function moveSelection(event) {
+    switch (event.keyCode) {
+        case 37:
+            leftArrowPressed();
+            break;
 
-                    case 39:
-                        rightArrowPressed();
-                    break;
-
-                    case 38:
-                        upArrowPressed();
-                    break;
-
-                    case 40:
-                        downArrowPressed();
-                    break;
-                }
-            };
-
-  function gameLoop()
-        {
-            moveRandomCircle();
-            setTimeout("gameLoop()",10);
+        case 39:
+            rightArrowPressed();
+            break;
+        case 38:
+            upArrowPressed();
+            break;
+        case 40:
+            downArrowPressed();
+            break;
+        case 32:
+            spaceKeyPressed();
+             break;
         }
+}
+function gameInit(){
+	listOfCircles = $(".circle");
+	for (i = 0; i < listOfCircles.length; i++) {
+		var left = i*100;
+		var top = i*100;
+		listOfCircles.eq(i).css('left',left);
+		listOfCircles.eq(i).css('top',top);
+		
+	}
+}
+function gameLoop()
+{	
+	if(!globalParameters.initiated){
+		gameInit();
+		globalParameters.initiated = true;
+	}
+    moveRandomCircle();
+    moveBullets();
+    displayParameters();
+    setTimeout("gameLoop()",10);
+ }
 
+function displayParameters(){
+	$('#parameters-life').html("LIFE: <b>" + spaceShipParams.life+"</b>");
+}
 
-    function moveRandomCircle(){
-         var element = $('#circle');
-         var dx = Math.random()-0.5;
-         var left = parseFloat(element.css("left")) - dx*2;
-         $('#circle').css('left',left);
+function moveRandomCircle(){
+    
+    var listOfCircles = $('.circle');
+    for (i = 0; i < listOfCircles.length; i++) {
+		var element = listOfCircles.eq(i);
+		var dx = Math.random()-0.5;
+		var left = parseFloat(element.css("left")) - dx*10;
+		if (left < 0){
+			left =  left + dx*20;
+		}
+		element.css('left',left);
 
-        var dy = Math.random()-0.5;
-        var top = parseFloat(element.css("top")) + dy*2;
-       $('#circle').css('top',top);
-    }
+		var dy = Math.random()-0.5;
+		var top = parseFloat(element.css("top")) - dy*10;
+		if (top < 0){
+			top =  top + dy*20;
+		}
+		element.css('top',top);
+	}
+
+}
+
+function moveBullets(){
+	
+	var listOfBullets = $('.bullet');
+	for (i = 0; i < listOfBullets.length; i++) {
+		var element = listOfBullets.eq(i);
+		var top = parseInt(element.css("top")) - 5;
+		element.css('top',top);
+		
+	}
+}
